02b326af010512b4119dddcfa011562586605e7b
xsabi <hcsabi@yahoo.com>
2020-03-30 14:00:16 +0200

    Preparing common page template
    creating
    - head.php
    - scripts.php
    - tail.php

---
+++ components/head.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Movie</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Materialize Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Main application stylesheet -->
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
---
+++ components/script.php
@@ -0,0 +1,5 @@
+<!-- jQuery scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  
